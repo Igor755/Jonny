9d53579de9de0135a1a18cf5cf2c6b0acc7a634c
i.metlin <metlin.igor@gmail.com>
2017-05-13 09:00:26 +0300

    90

--- src/main/java/Dream.java
+++ src/main/java/Dream.java
@@ -1,10 +1,28 @@
 package main.java;
 
+import java.util.Scanner;
 /**
  * Created by i.metlin on 11.05.2017.
  */
 public class Dream {
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double s1,s2;
+        String s3;
+        System.out.println("enter 1 zn");
+        s1 = scanner.nextDouble();
+
+        System.out.println("enter 2 zn");
+        s2 = scanner.nextDouble();
+
+        s3 = Double.toString((s1 + s2));
+        //if(s3)
+            //s3.append(s3.charAt("0."));
+        System.out.println(s3);
+
+
 
     }
 }
---
+++ src/main/java/Impl.java
@@ -0,0 +1,8 @@
+package main.java;
+
+/**
+ * Created by i.metlin on 12.05.2017.
+ */
+public interface Impl {
+
+}
