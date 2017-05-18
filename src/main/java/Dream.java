c1bfff169f5cfeddb87e9a14c7f4a9eea25c869c
i.metlin <metlin.igor@gmail.com>
2017-05-18 09:42:26 +0300

    Calculation

--- src/main/java/Dream.java
+++ src/main/java/Dream.java
@@ -14,7 +14,9 @@ public class Dream {
 */
 
         NumberFormat nf = new DecimalFormat("#.######");
-        //работает через запяту, через точку выбивает Exception()
+        //работает через запяту, через точку выбивает Exception
+        //http://www.sql.ru/forum/942830/doube-parse-hitryy-format
+        //http://www.cyberforum.ru/java-j2se/thread1815171.html
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("first");
